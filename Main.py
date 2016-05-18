b5202dcb641d7034f9d212ca1940a4e9dfd7f451
carloserodriguez2000 <carloserodriguez2000@yahoo.com>
2016-05-18 12:15:03 -0500

    program to shift a vector 3 times. Code can be updated to shift n number of times any vector size.

---
+++ Main.py
@@ -0,0 +1,33 @@
+################################################################################
+#
+def shiftRight(aList,indexList,listLen):
+    elementZero = aList[listLen-1]      # Last  element in the list. pop out
+    for shifter in indexList:
+        if shifter != 0 :
+            aList[shifter] = aList[shifter-1] # Shift all to the right. Note: negative index is cool but will avoid at this time
+    aList[0] = elementZero
+ 
+
+################################################################################
+#
+def shiftVector(aList, shiftTimes):
+    listLen   = len(aList)
+    indexList =list(range(listLen-1,-1,-1))  # Build reverse indexer list 4,3,2,1,0  last is always 0
+    
+    for i in indexList :            # Shift vector many times
+        if shiftTimes > 0 :
+            shiftRight(aList,indexList,listLen) # Shift vector right one time.
+            shiftTimes-=1
+
+################################################################################
+#
+def main():
+    aList = ['a','b','c','d']   # list of items to shift
+    shiftTimes = 3              # How many time to shift
+    shiftVector(aList, shiftTimes) # Go Shift vector that many times
+
+
+    print (aList)
+
+main()
+
